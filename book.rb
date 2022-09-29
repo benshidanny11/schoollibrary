1471dd7370907b1003b4b5ee51cabf9256aef9df
benshidanny11 <benshidanny11@gmail.com>
2022-09-29 10:09:25 +0200

    Implement review feedback

--- book.rb
+++ book.rb
@@ -7,7 +7,7 @@ class Book
     @rentals = []
   end
 
-  def add_rental(date, person)
-    @rentals.push(Rental.new(date, self, person))
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
